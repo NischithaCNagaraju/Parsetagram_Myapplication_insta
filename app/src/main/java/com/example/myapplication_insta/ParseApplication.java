9d2515442e033fa7738cf45f87dfa95ac879bc4c
NischithaCNagaraju <nischithacn@gmail.com>
2021-02-28 17:13:29 -0800

    Update at 4

--- app/src/main/java/com/example/myapplication_insta/ParseApplication.java
+++ app/src/main/java/com/example/myapplication_insta/ParseApplication.java
@@ -1,12 +1,17 @@
 package com.example.myapplication_insta;
 
 import android.app.Application;
+
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("VmLZJ15TzOitBycHhn3gsXyCZMsBstTuk1MrtpS8")
--- app/src/main/java/com/example/myapplication_insta/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/myapplication_insta/ui/login/LoginActivity.java
@@ -1,32 +1,17 @@
 package com.example.myapplication_insta.ui.login;
 
-import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import com.example.myapplication_insta.R;
-import com.example.myapplication_insta.ui.login.LoginViewModel;
-import com.example.myapplication_insta.ui.login.LoginViewModelFactory;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
@@ -44,14 +29,16 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
         if(ParseUser.getCurrentUser() != null)
         {
             goMainActivity();
         }
 
+
         etUsername = findViewById(R.id.username);
         etPassword = findViewById(R.id.password);
-        btnLogin = findViewById(R.id.login);
+        btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -83,7 +70,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private void goMainActivity()
     {
-        Intent i = new Intent(this, MainActivity);
+        Intent i = new Intent(this, com.example.myapplication_insta.MainActivity.class );
         startActivity(i);
         finish();
     }
